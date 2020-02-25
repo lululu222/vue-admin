671446995dc6d826a1aab4ce09a1e7c075f5696c
lululu222 <1104141372@qq.com>
2020-02-25 17:31:35 +0800

    测试

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -40,7 +40,7 @@
                             <span>权限管理</span>
                         </template>
                         <el-menu-item-group>
-                            <el-menu-item index="1-1">
+                            <el-menu-item index="roles">
                                 <i class="el-icon-camera-solid"></i>
                                 <span>角色列表</span>
                             </el-menu-item>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Login from '@/components/login/login'
 import Home from '@/components/home/home'
 import Users from '@/components/users/users'
+import Roles from '@/components/permission/roles'
 Vue.use(Router)
 
 export default new Router({
@@ -22,6 +23,11 @@ export default new Router({
           name: 'users',
           path: '/users',
           component: Users
+        },
+        {
+          name: 'roles',
+          path: '/roles',
+          component: Roles
         }
       ]
 
